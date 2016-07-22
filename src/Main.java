faa56186f9ca043fc4a0cf1f5c9eb380d100d4df
DumpSushi <DumpSushi@users.noreply.github.com>
2016-07-22 16:05:43 +0900

    練習6-2 3分割。

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,13 @@
+import comment.Zenhan;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception
 	{
 		// TODO 自動生成されたメソッド・スタブ
-		doWarusa();
-		doTogame();
-		callDeae();
-		showMondokoro();
-	}
-
-	public static void doWarusa()
-	{
-		System.out.println("warusa");
-	}
-
-	public static void doTogame()
-	{
-		System.out.println("togame");
-	}
-
-	public static void callDeae()
-	{
-		System.out.println("deae");
-	}
-
-	public static void showMondokoro() throws Exception
-	{
-		System.out.println("mondokoro");
-		doTogame();
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
 	}
 }
---
+++ src/comment/Kouhan.java
@@ -0,0 +1,16 @@
+package comment;
+
+public class Kouhan {
+
+	public static void callDeae()
+	{
+		System.out.println("deae");
+	}
+
+	public static void showMondokoro() throws Exception
+	{
+		System.out.println("mondokoro");
+		Zenhan.doTogame();
+	}
+
+}
